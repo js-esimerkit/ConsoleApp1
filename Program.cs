22484fb977d057ebea15319220332caa3a03c1e7
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-01-08 18:29:50 +0200

    esimerkki valmis

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname;
+            int age;
+            Console.WriteLine("Anna nimesi");
+            fname = Console.ReadLine();
+            Console.WriteLine("Kuinka vanha olet?");
+            //seuraava kaataa sovelluksen jos laittaa vahingossa kirjaimia
+            //age=int.Parse(Console.ReadLine());
+            //joten TryParse on tässä parempi
+            int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("Terve " + fname);
+            Console.WriteLine("Olet siis " + age + " vuotias");
+        }
+    }
+}
